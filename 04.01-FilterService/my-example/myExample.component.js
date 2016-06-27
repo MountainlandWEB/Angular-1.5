ebdf8f3e51b279b721f8db71b60ef9155d8d57ec
tdhale <tdhale@xactware.com>
2016-06-27 15:12:01 -0600

    added filter and scope

---
+++ 04.01-FilterService/my-example/myExample.component.js
@@ -0,0 +1,24 @@
+(function(){
+
+    angular.module('myApp')
+        .component('myExample', { // the tag for using this is <my-example></my-example>
+            templateUrl: "my-example/my-example.html",
+            controller: myExampleController
+        });
+
+    function myExampleController($filter, $scope) {
+        // put all code for this component in here (click handlers, component setup, UI-related code)
+        var self = this;
+        self.name = $filter('uppercase')('Hansel');
+        self.doChange = doChange;
+
+        function doChange() {
+            self.name = $filter('uppercase')(self.name);
+        }
+
+        // $scope.$watch("$ctrl.name", function(newval, oldval) {
+        //     self.name = $filter('uppercase')(newval);
+        // });
+    }
+
+})();
