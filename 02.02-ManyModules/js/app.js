952e91b48b4d1bacdab3c1e5f887fe4d133e089f
tdhale <tdhale@xactware.com>
2016-06-26 23:01:15 -0600

    added templates, DI, and services

--- 02.02-ManyModules/js/app.js
+++ 02.02-ManyModules/js/app.js
@@ -2,7 +2,7 @@
 
     // this CREATES the myApp module (because it has the second, array parameter)
     angular.module('myApp', [
-        /* inject/import modules here */
+        /* list imported modules here */
         'myExampleModule'
     ]);
 
