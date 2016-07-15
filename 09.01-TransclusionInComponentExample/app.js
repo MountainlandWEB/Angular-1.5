50433c85bc8ab93403ee2ae9c138b1bdf0de796e
Alberto Garza <agarza@xactware.com>
2016-07-15 00:18:15 -0600

    added transclusion example

--- 09.01-TransclusionInComponentExample/app.js
+++ 09.01-TransclusionInComponentExample/app.js
@@ -1,5 +1,16 @@
 (function () {
 
-    angular.module('app', []);
-    
+    angular.module('app', [])
+        .component('card', {
+            bindings: {
+                artist: '@'
+            },
+            template:
+                '<div>' +
+                '  <h3>{{$ctrl.artist || "No artist name"}}</h3>' +
+                '  <i><ng-transclude>No song title</ng-transclude></i>' +
+                '</div>',
+            transclude: true
+        });
+
 })();
