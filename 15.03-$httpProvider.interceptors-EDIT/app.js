2f505ce458b9ae109208d91d88fd573cdbec1785
mattpowell <piconation@yahoo.com>
2016-07-27 10:19:45 -0600

    7.27.16

--- 15.03-$httpProvider.interceptors-EDIT/app.js
+++ 15.03-$httpProvider.interceptors-EDIT/app.js
@@ -28,3 +28,17 @@
     }
 
 })();
+
+// Factory is an angular service that can be a global service to be called many times. Once you create a function for
+// your promise, you must then add it to the service by creating a self variable under the service.
+
+function getPhoto(id) { // This takes the id input and returns the promise with photo data.
+    var photo = undefined;
+    var deferred = $q.defer(); // This creates the promise. $q is a wrapper that includes the promise functionality
+                                // and other functionality. The promise is the variable deferred.
+    var photo = Character.get({id: photoId}) // This uses our $resource factory(service).
+        .$promise.then(function(data) { // This gets the data from the $resource promise
+        deferred.resolve(data); // This stores the data in the promise.
+    });
+    return deferred.promise; // This sends the promise back with the data.
+}
