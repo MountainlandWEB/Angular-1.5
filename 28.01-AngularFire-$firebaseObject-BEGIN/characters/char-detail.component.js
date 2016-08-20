b9746342efc6693569340090eb4b2835aa4be9c1
Alberto Garza <betogf@gmail.com>
2016-08-20 11:36:01 -0600

    added end folder for 28.01

--- 28.01-AngularFire-$firebaseObject-BEGIN/characters/char-detail.component.js
+++ 28.01-AngularFire-$firebaseObject-BEGIN/characters/char-detail.component.js
@@ -25,20 +25,20 @@
     function charDetailController() {
         var vm = this;
 
-        vm.user = {};
-        vm.user.name = '';
-        vm.user.height = 0;
-        vm.user.mass = 0;
+        vm.char = {};
+        vm.char.name = '';
+        vm.char.height = 0;
+        vm.char.mass = 0;
 
         vm.save = function() {
             console.log('saving...');
-            console.log(JSON.stringify(vm.user));
+            console.log(JSON.stringify(vm.char));
             console.log('TODO: Finish save implementation here');
         }
 
         vm.delete = function() {
             console.log('deleting...');
-            console.log(JSON.stringify(vm.user));
+            console.log(JSON.stringify(vm.char));
             console.log('TODO: Finish delete implementation here');
         }
     }
