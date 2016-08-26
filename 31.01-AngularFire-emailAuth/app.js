a7908cd33a96bd383210cafc7a846d5bb149cf29
tdhale <tdhale@xactware.com>
2016-08-25 22:06:01 -0600

    added 31 ngDialog

---
+++ 31.01-AngularFire-emailAuth/app.js
@@ -0,0 +1,22 @@
+(function(){
+
+    // this CREATES the myApp module (because it has the second, array parameter)
+    angular.module('app', [
+        /* listed imported modules here */
+        'ui.router',
+        'ngMaterial',
+        /* Step #3: include the firebase module in the module dependency list for your app */
+        'firebase',
+        'ngStorage',
+        'ngDialog'
+    ])
+        .config(myAppConfig);
+
+    function myAppConfig($urlRouterProvider, $mdThemingProvider) {
+        $urlRouterProvider.otherwise('/main');
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('red');
+    }
+
+})();
---
+++ 31.02-ngDialog/characters/char-list.component.js
@@ -0,0 +1,72 @@
+(function(){
+
+    angular.module('myApp')
+        .component('charList', { // the tag for using this is <char-list>
+            templateUrl: "characters/char-list.component.html",
+            controller: charListController
+        })
+        .config(charListConfig)
+        .controller('charDetailController', charDetailController);
+
+    function charListConfig($stateProvider) {
+        $stateProvider.state('main', {
+            url: '/main',
+            template: '<char-list></char-list>'
+        });
+    }
+
+    function charListController(characterService, ngDialog, $scope) {
+
+        // variables
+        var self = this;
+        self.orderBy = 'name';
+        self.sortClass= 'sort-asc';
+        self.columns = ['name','gender','mass'];
+        self.selectedChar = characterService.selectedChar;
+        self.characters = characterService.characters;
+
+        // functions
+        self.sort = sort;
+        self.selectChar = selectChar;
+        self.close = close;
+
+        function sort(attribute) {
+            self.sortClass = 'sort-asc'; // down arrow
+            var newOrderBy = attribute;
+            if (self.orderBy === attribute) {
+                newOrderBy = '-' + attribute;
+                self.sortClass = 'sort-desc'; // up arrow, reverse sort
+            }
+            self.orderBy = newOrderBy;
+        }
+
+        function selectChar(char) {
+            self.selectedChar = characterService.selectedChar = char;
+            var dialog = ngDialog.open({
+                template: 'characters/char-detail.component.html',
+                controller: 'charDetailController',
+                controllerAs: '$ctrl',
+                data: {
+                    character: char
+                }
+            });
+            dialog.closePromise.then(close);
+        }
+
+        function close() {
+            self.selectedChar = characterService.selectedChar = undefined;
+        }
+
+    }
+
+    function charDetailController() {
+        var self = this;
+        self.close = close;
+
+        function close() {
+            self.onClose(); // call output binding
+        }
+    }
+
+
+})();
